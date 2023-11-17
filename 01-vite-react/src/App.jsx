81aca6b49abdf79be785ff8cb9de91b31b92d56f
Faiz Ansari <faiz.faiz1920@gmail.com>
2023-11-17 17:00:44 +0530

    adding variables

--- 01-vite-react/src/App.jsx
+++ 01-vite-react/src/App.jsx
@@ -1,10 +1,12 @@
 import NewComponent from "./NewComponent";
 
 function App() {
+  const username = "Faiz Ansari";
   return (
     <>
       <h1> Vite React </h1>
       <NewComponent></NewComponent>
+      <h3> Hi {username}</h3>
     </>
   );
 }
